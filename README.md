042545402c5f512000107cac6b140ccaee734066
Wieteke Öztürk-Pots <wieteke.ozturkpots@valtech.nl>
2017-10-09 10:29:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# CogBots
+SideNote:
+
+- Als frontend developer kan ik niet bij de API's. Ik lift nu even mee op Guido's account. Dat houdt ook in dat ik 
+- het mooie is dat er ook veel .NET documentatie is en koppelingen. Dat is mooi als we met eigen endpoints gaan werken.
+
+Onder de AI services van onder andere Cognitive Services en de Azure Bot Service
+
+Azure Bot Service:
+integratie van de website naar email/skype slack/etc
+
+Ik ben even doorgegaan op een simpele QnA voor VVV mbv de bot framework
+
+
+
+
+Toepassingen die ik gezien heb: first install voor ikea app met een leuke chat- achtige manier voor interactie.
+
+Cognitive Services kunnen hierbij ondersteunen door zien, horen en interpreteren van doorgezonden data.
+
+Er zijn verschillende cognitive services:
+
+Vision
+	identify (information) images
+
+Knowledge
+	map complex information (semantic search / recommendations)
+
+Language
+	process natural language evaluate recognize needs of user
+
+Speach
+	convert audio to verification / speaker recognition
+
+Search
+	Bing search API, meh
+
+
+ waarbij recommendations misschien leuk is om verder uit te zoeken. voor onze klanten, voor bijvoorbeeld related items achtige constructies
---
+++ app.js
@@ -0,0 +1,39 @@
+let recommmendations = 'Endpoint: https://westus.api.cognitive.microsoft.com/recommendations/v4.0',
+		keyOne = ' c046560bd2f34d7396881a2616a4ed19',
+		keyTwo = '3796c08e7820457d97cce876d94e800f';
+
+
+//botbuilder let's us run a bot locally.
+let builder  =  require('botbuilder');
+// Restify let's us use the bot in a framwork channel or emulator
+let restify = require('restify');
+
+//setup Restify Server 
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+	console.log('%s listening to %s', server.name, server.url);
+});
+
+
+//we start a listener. without restify we use 'new builder.ConsoleConnector().listen();'
+//with restiy , the listener is added below
+let connector = new builder.ChatConnector({
+		// you can use the framework service in the emulator without MS App credentials
+	  appId: 'aeeb7b31-fc4c-43f8-9e00-ff3a97f7a898',
+    appPassword: 'v5siPE79fS94EXxOGcC2HR9'
+
+   //for REST with the framework portal
+   //Appname: AzureBot
+   //App ID: 
+   //App password: 
+});
+
+
+server.post('/api/messages', connector.listen());
+
+//and it wil execute this function on enter. and console.log what we typed. very handy for nothing
+
+//the UniversalBot is the brains of the app.manages all conversation.
+let bot = new builder.UniversalBot(connector, session => {
+	session.send("You said: %s", session.message.text);
+});
