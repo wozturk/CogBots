6aafb8f8de393335efe0bd1cb638d457d0ee4fc9
Wieteke Öztürk-Pots <wieteke.ozturkpots@valtech.nl>
2017-11-20 12:19:21 +0100

    mss zo

--- app.js
+++ app.js
@@ -106,13 +106,13 @@ bot.on('error', function (e) {
 });
 
 // initiating the root dialog
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded) {
-        message.membersAdded.forEach(function (identity) {
-            if (identity.id === message.address.bot.id) {
+// bot.on('conversationUpdate', function (message) {
+//     if (message.membersAdded) {
+//         message.membersAdded.forEach(function (identity) {
+//             if (identity.id === message.address.bot.id) {
 
-                bot.beginDialog(message.address, '/');
-            }
-        });
-    }
-});
+//                 bot.beginDialog(message.address, '/');
+//             }
+//         });
+//     }
+// });
